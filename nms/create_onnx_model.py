b3966db41953187ecb9a12e3f249fc00f3926f37
Yousif Masoud <yousifmasoud8@gmail.com>
2025-10-07 22:49:07 +0300

    Fixed nms onnx parameters

--- nms/create_onnx_model.py
+++ nms/create_onnx_model.py
@@ -1,11 +1,22 @@
 import onnx
 import os
+import sys
 from src.onnx_nms import create_onnx_nms_model
 
 if __name__ == "__main__":
     output_dir = "./output_files"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    model = create_onnx_nms_model()
+
+    if len(sys.argv) >= 4:
+        max_output_boxes_per_class = int(sys.argv[1])
+        iou_threshold = float(sys.argv[2])
+        score_threshold = float(sys.argv[3])
+    else:
+        max_output_boxes_per_class = 50
+        iou_threshold = 0.5
+        score_threshold = 0.1
+
+    model = create_onnx_nms_model(max_output_boxes_per_class, iou_threshold, score_threshold)
     model.ir_version = 10
     onnx.save(model, os.path.join(output_dir, "nms.onnx"))
